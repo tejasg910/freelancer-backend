e1a328fa589d7b6ff2f2d4bac6a81a21d2549329
tejasg910 <tejasgiri@ipangram.com>
2024-02-05 11:55:19 +0530

    added

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(multer().any());
 
 corsOptions = {
-  origin: "http://localhost:3000", // Change this to match your frontend URL
+  origin: process.env.FRONTEND_ORIGIN, // Change this to match your frontend URL
   credentials: true,
 };
 
